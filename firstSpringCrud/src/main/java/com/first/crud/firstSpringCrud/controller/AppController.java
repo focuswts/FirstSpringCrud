661c370c243efe414364617f5bc9b79e2c08ce99
1676886 <1676886@07HW012072.SOAM.TCS.com>
2019-04-09 13:33:39 -0300

    CRUD Completo

--- firstSpringCrud/src/main/java/com/first/crud/firstSpringCrud/controller/AppController.java
+++ firstSpringCrud/src/main/java/com/first/crud/firstSpringCrud/controller/AppController.java
@@ -1,17 +1,49 @@
 package com.first.crud.firstSpringCrud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.first.crud.firstSpringCrud.entitys.User;
+import com.first.crud.firstSpringCrud.repository.UserRepository;
 
 @Controller
 public class AppController {
 
+	private final UserRepository userService;
+
+	@Autowired
+	public AppController(UserRepository userService) {
+		this.userService = userService;
+	}
+
 	@RequestMapping("/")
 	public String index(Model model) {
-		model.addAttribute("title", "Titulo Crud");
+		List<User> usuarios = (List<User>) userService.findAll();
+		model.addAttribute("title", "Form Inserir Usuário");
+		model.addAttribute("user", new User());
+		model.addAttribute("users", usuarios);
+		model.addAttribute("userUpdate", new User());
 		return "index";
 
 	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public String update(@ModelAttribute("userUpdate") User userUpdate, Model model) {
+//		System.out.println("User: " + userUpdate.getId());
+//		System.out.println("Username: " + userUpdate.getUsername());
+//		System.out.println("Email: " + userUpdate.getEmail());
+		model.addAttribute("title", "Form Atualizar Usuário");
+		model.addAttribute("userUpdate", userUpdate);
+		return "indexUpdate";
+
+	}
+	
+	
 
 }
--- firstSpringCrud/src/main/java/com/first/crud/firstSpringCrud/controller/RestController.java
+++ firstSpringCrud/src/main/java/com/first/crud/firstSpringCrud/controller/RestController.java
@@ -2,9 +2,13 @@ package com.first.crud.firstSpringCrud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.first.crud.firstSpringCrud.entitys.User;
@@ -17,23 +21,35 @@ public class RestController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@GetMapping("/add")
-	public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String password,
-			@RequestParam String email) {
-
-		User user = new User();
-		user.setUsername(name);
-		user.setPassword(password);
-		user.setEmail(email);
+	@PostMapping(value = "/add")
+	public String addNewUser(@ModelAttribute("user") User user) {
 
 		userRepository.save(user);
-		return "Saved";
+		return "redirect:../";
 	}
 
 	@GetMapping("/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
 
 		return userRepository.findAll();
+	}
+
+	@PutMapping(value = "/update")
+	public String editUser(@ModelAttribute("userUpdate") User userUpdate, Model model) {
+		System.out.println("Userupdate:" + userUpdate.getId());
+		System.out.println("UserUpdate" + userUpdate.getUsername());
+
+		userRepository.save(userUpdate);
+		model.addAttribute("title", "Form Inserir Usuário");
+
+		return "redirect:../";
+	}
+
+	@DeleteMapping("/delete")
+	public String deleteUser(@ModelAttribute("userDelete") User userDelete) {
+		System.out.println("Delete");
+		userRepository.delete(userDelete);
+		return "redirect:../";
 	}
 
 }
